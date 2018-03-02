b619145759fb189cc20c872dbfec4512a9f0ebea
Silas <silasplfilho@outlook.com>
2018-03-02 15:49:31 -0300

    atualizacao 02_03

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -5,8 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('/home/silas/Documents/GIT/Python-Codes/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
