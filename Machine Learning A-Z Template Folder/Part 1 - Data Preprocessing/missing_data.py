0801a3830dd21b70a17cd30f37fe9f84fef8e1bb
Silas <silasplfilho@outlook.com>
2018-03-02 14:57:08 -0300

    atualizacao

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/missing_data.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/missing_data.py
@@ -1,16 +1,18 @@
 # Data Preprocessing
 
-
+#%%
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#%%
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('/home/silas/Documents/GIT/Python-Codes/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
+#%%
 # Taking care of missing data
 from sklearn.preprocessing import Imputer # importando a classe Imputer q trata de dados faltosos
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
