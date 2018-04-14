7eb47fca8e468a8c53e32beabf0554da7c1d1229
Silas <silasplfilho@outlook.com>
2018-04-14 11:25:59 -0300

    algo nao pushado

--- Variados/leitura_bibliografia.py
+++ Variados/leitura_bibliografia.py
@@ -4,5 +4,7 @@ path = r'/home/silas/Documents/GIT/Python-Codes/Variados/prior keywords.csv'
 biblio = pd.read_csv(path, sep=';', header = None, names = ['title', 'keywords'])
 
 pd.unique(biblio.keywords)
-biblio.keywords.split(',')
+biblio.keywords.iloc[1]
+for i in biblio.keywords: biblio.keywords.loc[i] = biblio.keywords.loc[i].split(',')
+
 .str.split(',')
