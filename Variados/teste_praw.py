28f247be3bcbef8bfe6fb7f290653a635266a6f0
silasplfilho <you@example.com>
2018-05-01 18:46:24 -0300

    update teste-praw

--- Variados/teste_praw.py
+++ Variados/teste_praw.py
@@ -6,36 +6,36 @@ reddit = praw.Reddit(client_id = 'cOrWaHLB_Hdidw', client_secret = '21usAKUK_6f7
 
 subreddit = reddit.subreddit('depression')
 #
+conversedict = {}
 hot_depression = subreddit.hot(limit = 3)
 
-for s in hot_depression:
-    if not s.stickied:
-        print('Title: {}, ups: {}, downs: {}, Have we visited?: {}'.format(s.title,
-        s.ups,
-        s.downs,
-        s.visited))
-    comments = s.comments
-    for comment in comments:
-        print(20*'-')
-        print(comment.body)
-        if len(comment.replies) > 0:
-            for reply in comment.replies:
-                print('REPLY:')
-                print("\t"+reply.body)
-
-hot_python = subreddit.hot(limit=3)
-for submission in hot_python:
+for submission in hot_depression:
     if not submission.stickied:
-        print('Title: {}, ups: {}, downs: {}, Have we visited?: {}, subid: {}'.format(submission.title,
-                                                                                                   submission.ups,
-                                                                                                   submission.downs,
-                                                                                                   submission.visited,
-                                                                                                   submission.id))
-        submission.comments.replace_more(limit=0)
-        # limiting to 15 results to save output
-        for comment in submission.comments.list()[:15]:
-            print(20*'#')
-            print('Parent ID:',comment.parent())
-            print('Comment ID:',comment.id)
-            # limiting output for space-saving-sake, feel free to not do this
-            print(comment.body[:200])
+        print('Title: {}, ups: {}, downs: {}, Have we visited?: {}, subid{}'.format(submission.title,
+        submission.ups,
+        submission.downs,
+        submission.visited,
+        submission.id))
+
+        submission.comments.replace_more(limit = 0)
+        for comment in submission.comments.list():
+            if comment.id not in conversedict:
+                conversedict[comment.id] = [comment.body, {}]
+                if comment.parent() != submission.id:
+                    parent = str(comment.parent())
+                    conversedict[parent][1][comment.id] = [comment.ups, comment.body]
+            #
+            # print(20*'#')
+            # print('Parent ID:',comment.parent())
+            # print('Comment ID:',comment.id)
+            # print(comment.body[:200])
+
+for post_id in conversedict:
+    message = conversedict[post_id][0]
+    replies = conversedict[post_id][1]
+    if len(replies) > 1:
+        print('Original Message: {}'.format(message))
+        print(35*'_')
+        print('Replies:')
+        for reply in replies:
+            print(replies[reply])
