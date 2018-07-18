01690e07598149acf06adc94774b0c76e6c08a2b
Silas <silasplfilho@outlook.com>
2018-07-18 19:07:37 -0300

    up

--- Python Bootcamp/python_OOP_tutorial.py
+++ Python Bootcamp/python_OOP_tutorial.py
@@ -2,6 +2,8 @@
 # https://www.youtube.com/watch?v=ZDa-Z5JzLYM&t=153s
 class Employee:
 
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -11,6 +13,8 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
 
 emp_1 = Employee('Corey', 'Schaefer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
@@ -18,3 +22,7 @@ emp_2 = Employee('Test', 'User', 60000)
 print(emp_1.email)
 print(emp_2.email)
 print(emp_1.fullname())
+emp_1.apply_raise()
+print(emp_1.pay)
+
+emp_1.raise_amount
