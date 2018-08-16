92594ea171790e510c8d130a0b450cdd38ee1368
Silas <silasplfilho@outlook.com>
2018-08-16 18:23:04 -0300

    update

--- Variados/Intro to ML_BOOK/Chapter 2/knn.py
+++ Variados/Intro to ML_BOOK/Chapter 2/knn.py
@@ -74,3 +74,24 @@ reg = KNeighborsRegressor(n_neighbors=3)
 reg.fit(X_train, y_train)
 
 print("Test set predictions:\n{}".format(reg.predict(X_test)))
+
+print("Test set R^2: {:.2f}".format(reg.score(X_test, y_test)))
+#-----
+fig, axes = plt.subplots(1, 3, figsize=(15, 4 ))
+# create 1,000 data points, evenly spaced between -3 and 3
+line = np.linspace(-3, 3, 1000).reshape(-1, 1 )
+for n_neighbors, ax in zip([1, 3, 9], axes ):
+    # make predictions using 1, 3, or 9 neighbors
+    reg = KNeighborsRegressor(n_neighbors=n_neighbors)
+    reg.fit(X_train, y_train)
+    ax.plot(line, reg.predict(line))
+    ax.plot(X_train, y_train, '^', c=mglearn.cm2(0), markersize=8)
+    ax.plot(X_test, y_test, 'v', c=mglearn.cm2(1), markersize=8)
+    ax.set_title ("{} neighbor(s)\n train score: {:.2f} test score: {:.2f}".format(n_neighbors, reg.score(X_train, y_train ),
+                reg.score(X_test, y_test)))
+    ax.set_xlabel("Feature")
+    ax.set_ylabel("Target")
+
+axes[0].legend(["Model predictions", "Training data/target", "Test data/target"], loc="best")
+plt.show()
+help(KNeighborsClassifier)
