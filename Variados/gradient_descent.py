ffcc48ebabcc08a2a158e328b51645099c123824
Silas <silasplfilho@outlook.com>
2018-08-22 19:10:58 -0300

    up

--- Variados/gradient_descent.py
+++ Variados/gradient_descent.py
@@ -1,9 +1,23 @@
 import numpy as np
 
+def step_gradient(b_current, m_curent, points, learningRate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points)
+
 def compute_error_for_given_points(b, m, points):
+    totalError = 0
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        totalError += (y - (m * x + b)) **2
+    return(totalError/float(len(points)))
+
 
 def step_gradient(b_current, m_curent, points, learning_rate):
     #gradient descent
+    b_gradient = 0
+    m_gradient = 0
 
 def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
     b = starting_b
@@ -11,7 +25,7 @@ def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_i
 
     for i in range(num_iterations):
         b, m = step_gradient(b, m, array(points), learning_rate)
-    return [b, m]
+    return([b, m])
 
 def run():
     points = genfromtext('data.csv', delimite=',')
