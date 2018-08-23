c2fc0af2247a1c7cfbe74dafb831017120781bcf
Silas <silasplfilho@outlook.com>
2018-08-23 18:52:49 -0300

    up

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/test.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/test.py
@@ -5,17 +5,16 @@ import psycopg2
 conn = psycopg2.connect(database="db_rais", user="i3geoconsulta", password="i3geoconsulta", host="10.3.40.10", port="5432")
 cur = conn.cursor()
 #------
-query="""
-    (select v.cbo2002, v.rem_med_r, v.grau_instr
-        from tb_vinculos_2016 as v
-        where ceiling(v.cbo2002/100) = (1113)
-        limit 100)
-"""
-select (2018 - cast(s.ano_ing_spub as integer)) as experiencia, v.rem_med_r, s.situacao_funcional
-    from tb_siape_v02 as s
-        join tb_vinculos_2016 as v
-        on s.cpf_servidor = v.cpf
-    where s.ano = 2017 and (v.cbo2002/100) = 1113
-    limit 2
+query= """select (2018 - cast(s.ano_ing_spub as numeric)) as experiencia, v.rem_med_r, s.situacao_funcional
+    from tb_siape_v02 as s join tb_vinculos_2016 as v
+    on s.cpf_servidor = v.cpf
+    where s.ano = 2016 and s.ano_ing_spub != 's/info'
+    limit 200"""
 
-select * from tb_siape_v02 limit 2
+cur.execute(query)
+data = pd.DataFrame(cur.fetchall(), columns=['experiencia', 'rem_med_r', 'situacao_funcional'])
+cur.close()
+conn.close()
+#----
+path = r"C:\\Users\\b247857261\\Desktop\\Scripts\\silas\\Python-Codes\\Machine Learning A-Z Template Folder\\Part 2 - Regression\\Section 5 - Multiple Linear Regression\\data.csv"
+data.to_csv(path, sep=",")
