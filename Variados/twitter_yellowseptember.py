d9c460f7c4cce8ef54f45b42f9881da3b8526717
Silas <silasplfilho@outlook.com>
2018-09-18 18:30:42 -0300

    update

--- Variados/twitter_yellowseptember.py
+++ Variados/twitter_yellowseptember.py
@@ -1,1 +1,9 @@
 from twitter_fetch.fetcher import fetch_loop
+
+from twitter_fetch import persistence
+
+persistence.DB = "yellowseptember"
+
+query = "#worldsuicidepreventionday since:2018-09-01 until:2018-09-18"
+
+fetch_loop(query)
