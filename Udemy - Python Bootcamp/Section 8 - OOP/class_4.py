c41925134aefad9974e8d45f7227e5891e6d5d01
Silas <silasplfilho@outlook.com>
2018-09-25 16:05:46 -0300

    up

---
+++ Udemy - Python Bootcamp/Section 8 - OOP/class_4.py
@@ -0,0 +1,23 @@
+class Book():
+
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+         return "{} by {}".format(self.title, self.author)
+
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print("A book object has been deleted.")
+#
+b = Book('Python rocks', 'Jose', 200)
+
+print(b)
+len(b)
+str(b)
+del b
+b
---
+++ Udemy - Python Bootcamp/Section 8 - OOP/homework.py
@@ -0,0 +1,13 @@
+class Line():
+    def __init__(self, coor1, coor2):
+        self.coor1 = coor1
+        self.coor2 = coor2
+
+    def distance():
+        part1 = (coor2[0] - coor1[0])**2
+        part2 = (coor2[1] - coor1[1])**2
+        d = sqrt(part1 + part2)
+        return
+
+    def slope():
+        part1 = 
