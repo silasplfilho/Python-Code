6186d593df2392a064666e52ec76ef52f2e98262
Silas Pereira Lima Filho <Silas.Pereira@ibm.com>
2019-10-31 17:12:23 -0300

    updating bs4 script

--- bs4Test/testBS4healingwell.py
+++ bs4Test/testBS4healingwell.py
@@ -1,6 +1,8 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import re
+import json
+import time
 # ----
 mainURL = "https://www.healingwell.com/community/default.aspx?f=19"
 source = urllib.request.urlopen(mainURL).read()
@@ -38,83 +40,43 @@ for pageIterator in range(3):
         dictLayout['title'] = thread.a.text
         dictLayout['views'] = thread.find('div', class_='views').text
         dictLayout['link'] = thread.find('a', class_='forum-title').get('href')
-        print(dictLayout)
+        # print(dictLayout)
         threadList.append(dictLayout.copy())
-
-
-threadComments = []
-commentDict = dict()
+# -----------------------------------
 
 for postIterator in threadList:
-    postLink = postIterator['link']
+    postLink = "https://www.healingwell.com" + postIterator['link']
     source = urllib.request.urlopen(postLink).read()
     soup = BeautifulSoup(source, 'html.parser')
 
-    # comentarios e timestamp do post
-    comments = soup.findAll('div', class_='post-body')
-    commentsPostDate = soup.findAll('div', class_='posted')
+    try:
 
-    # postIterator['comment'] = 
-    # commentDict['comment'] = 
+    except:
 
-    for i, j in zip(comments, commentsPostDate):
-    print("__________JA ACABOU JESSICA")
-    print(j.text)
-    print(i.text)
+    # autor, timestamp e comentarios do post
+    smthng = soup.findAll(['a', 'div'], class_=['user-name', 'post-body', 'posted'])
+
+    threadComments = []
+    commentDict = dict()
+
+    for i in range(0, len(smthng), 3):
+        commentDict['commentAuthor'] = smthng[i].text  # Autor do Comentario
+        commentDict['commentTimestamp'] = smthng[i+1].text  # Data da publicacao
+        commentDict['comment'] = smthng[i+2].text  # Texto do Comentario
+
+        threadComments.append([commentDict['commentAuthor'],
+                               commentDict['commentTimestamp'],
+                               commentDict['comment']])
+
+    postIterator['postContent'] = threadComments
+    # print(postIterator['postContent'])
 
 # ---------------------------
-postLink = threadList[0]['link']
-postLink = "https://www.healingwell.com"+ postLink
-source = urllib.request.urlopen(postLink).read()
-soup = BeautifulSoup(source, 'html.parser')
+threadList[0]['last_date']
 
-comments = soup.find('div', class_='post-body')
-
-while comments.findNext('div', class_='post-body') is not None:
-    commentBody = comments.text.split('\r\n')[1]
-    comments = comments.findNext('div')
+with open('bs4Test/testHealingWell.json', 'w') as file:
+    json.dump(threadList, file)
+# type(threadList)
 
 
-
-
-
-
-commentsPostDate = soup.find('div', class_='posted').text
-
-
-
-
-# ----------------------------
-source = urllib.request.urlopen(postLink).read()
-soup = BeautifulSoup(source, 'html.parser')
-# /community/default.aspx?f = 19 & m = 4141476
-# ---
-# tentando chegar direto no div que tem a informacao necessaria
-focusedDiv = soup.find('div', class_='section-body-secondary')
-
-threadDiv = soup.findAll('div', class_=re.compile("row fugazi forum-list"))
-threadDiv[0].text.replace('^\n', '')
-
-threadDiv[0].text
-threadDiv[0].find('a', class_='forum-title').get('href')
-# ---
-# criando uma lista, onde cada elemento é um dicionario/thread
-threadList = []
-dictLayout = dict()
-
-for thread in threadDiv:
-    dictLayout['last_date'] = thread.find('div', class_='last-comment-date').text
-    dictLayout['author']    = thread.p.text
-    dictLayout['title']     = thread.a.text
-    dictLayout['views']     = thread.find('div', class_='views').text
-    dictLayout['link']      = thread.find('a', class_='forum-title').get('href')
-    print(dictLayout)
-    threadList.append(dictLayout.copy())
-
-
-
-threadList[0]
-threadList[0]['link']
-
-paging_link = paging[0].find_all('a', {'class': 'paging__link'})
-last_page = int([item.get('href').split('/')[-1] for item in paging_link][-1])
+# ---------------------------
